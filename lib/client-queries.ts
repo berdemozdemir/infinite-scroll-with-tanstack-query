575105bf0703a9ad26ebf7b3c5b8a7d74d4eb566
berdemozdemir@gmail.com <berdemozdemir@gmail.com>
2025-09-30 16:14:57 +0300

    refactor: add return type to fetch items function in order to solve any type issue

--- lib/client-queries.ts
+++ lib/client-queries.ts
@@ -1,9 +1,26 @@
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { fetchItems } from "./fetch-items";
+import { Item } from '@/app/api/items/route';
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+type ItemsResponse = {
+  data: Item[];
+  nextPage: number | null;
+};
+
+export async function fetchItems({
+  pageParam,
+}: {
+  pageParam: number;
+}): Promise<ItemsResponse> {
+  const res = await fetch(`/api/items?page=${pageParam}`);
+
+  if (!res) throw new Error('Something went wrong while fetching items');
+
+  return await res.json();
+}
 
 export const useFetchItemsInfiniteQuery = () =>
   useInfiniteQuery({
-    queryKey: ["items"],
+    queryKey: ['items'],
     queryFn: fetchItems,
     initialPageParam: 0,
     getNextPageParam: (lastpage) => lastpage.nextPage,
